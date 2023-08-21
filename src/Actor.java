b2d6b7fdfab96fac56064c61b6e28532fa1d5b18
Gabriel Zappe <129631016+GabrielZappe@users.noreply.github.com>
2023-08-21 00:34:35 -0300

    Added the Actor and Director class!

--- src/Actor.java
+++ src/Actor.java
@@ -1,17 +1,8 @@
 public class Actor extends Person {
     private String[] famousRoles;
 
-    public Actor(String name, int age, String nationality, double height, String gender, String[] famousRoles) {
+    public Actor(String name, int age, String nationality, double height, String gender) {
         super(name, age, nationality, height, gender);
-        this.famousRoles = famousRoles;
-    }
-
-    public String[] getFamousRoles() {
-        return famousRoles;
-    }
-
-    public void setFamousRoles(String[] famousRoles) {
-        this.famousRoles = famousRoles;
     }
 
     @Override
--- src/Director.java
+++ src/Director.java
@@ -1,17 +1,8 @@
 public class Director extends Person {
     private String[] directedMovies;
 
-    public Director(String name, int age, String nationality, double height, String gender, String[] directedMovies) {
+    public Director(String name, int age, String nationality, double height, String gender) {
         super(name, age, nationality, height, gender);
-        this.directedMovies = directedMovies;
-    }
-
-    public String[] getDirectedMovies() {
-        return directedMovies;
-    }
-
-    public void setDirectedMovies(String[] directedMovies) {
-        this.directedMovies = directedMovies;
     }
 
     @Override
